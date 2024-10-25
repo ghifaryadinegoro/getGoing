4234d53a932ecebf7dd6e8656ea679c5aa1fa8e3
Ghifary Adinegoro <ghifaryadinegoro555@gmail.com>
2024-10-25 20:46:50 +0700

    fix translation animation

--- src/components/CityCard.jsx
+++ src/components/CityCard.jsx
@@ -2,7 +2,7 @@ export default function CityCard(props) {
   const { city } = props;
 
   return (
-    <div className="p-2">
+    <div className="p-2 hover:-translate-y-2 duration-300 cursor-pointer">
       <div
         className="w-full h-80 bg-cover bg-center rounded-xl flex flex-col justify-end p-4 relative"
         style={{ backgroundImage: `url(${city.image})` }}
